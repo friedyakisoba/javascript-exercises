09a4e3b725eb48fd5ea5b2c45cd710094b9c6491
friedyakisoba <142198791+friedyakisoba@users.noreply.github.com>
2024-12-20 18:42:42 -1000

    finished 1-4

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+const reverseString = function(word) {
+let inputString = "";
 
+for (let int = word.length - 1; int >= 0; int--) {
+    inputString += word[int];
+}
+return inputString;
 };
 
 // Do not edit below this line

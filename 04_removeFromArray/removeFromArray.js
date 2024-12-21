5d15f6ad7e9f59250e87082955e2afcea98d4d52
friedyakisoba <142198791+friedyakisoba@users.noreply.github.com>
2024-12-21 13:19:04 -1000

    finished 04/05

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,32 @@
-const removeFromArray = function() {
+const removeFromArray = function(inputArray, ...targetValue) {
+    let storeArray = [];
+    
+    inputArray.forEach((item) => {
+        if(!targetValue.includes(item)) {
+            storeArray.push(item);
+        }
+    });
+    
+   return storeArray;
 };
+
+
+// original solution
+// const removeFromArray = function(inputArray, ...targetValue) {
+//     let storeArray = [];
+
+//     if (!Array.isArray(inputArray) || inputArray === undefined ) return "ERROR";
+
+//     for (let i = 0; i < inputArray.length; i++) {
+//         if (!targetValue.includes(inputArray[i])) {
+         
+//             storeArray.push(inputArray[i]);
+//         }
+//     }
+//     return storeArray;
+// };
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,25 @@
-const sumAll = function() {
+const sumAll = function (...intSum) {
+    let finalValue = 0;
 
+    for (let i = 0; i < intSum.length; i++) {
+        if (intSum[i] < 0 ||
+            typeof intSum[i] !== 'number' ||
+            intSum[i] % 1 !== 0) return "ERROR";
+
+
+    }
+
+    for (let i = Math.min(null, ...intSum); i <= Math.max(null, ...intSum); i++) {
+
+        finalValue += i;
+
+    }
+    return finalValue;
 };
+
+
+// throw an error
+
 
 // Do not edit below this line
 module.exports = sumAll;
